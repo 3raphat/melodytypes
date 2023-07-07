a6e9044a12066f4ac2b8e9f3870f0a59c4c304ce
Theeraphat J <96657413+3raphat@users.noreply.github.com>
2023-07-07 04:35:56 +0000

    remove unnecessary scopes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,10 +8,6 @@ const env = cleanEnv(process.env, {
 })
 
 const scopes = [
-  'playlist-modify-private',
-  'playlist-modify-public',
-  'playlist-read-collaborative',
-  'playlist-read-private',
   'user-read-email',
   'user-read-private',
   'user-top-read',
